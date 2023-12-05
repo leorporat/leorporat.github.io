230bb3a8f7cfb0e96830efa97932bff48708c4ca
Leor Porat <lporat874@student.fuhsd.org>
2023-12-04 18:55:08 -0600

    update favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Sidebar from './Sidebar.jsx'
 import './App.css'
 import Home from './Views/Home.jsx'
@@ -8,7 +5,6 @@ import Projects from './Views/Projects.jsx'
 import {Route, Routes} from "react-router-dom"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
