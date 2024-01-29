e1385dcf1f4509de79a9c2e4b30a10db128e9288
Leor Porat <lporat874@student.fuhsd.org>
2024-01-28 18:05:10 -0600

    Fix small grammar mistake

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
         <p id='interests'>I'm interested in <strong>software engineering, mathematics, and the intersection
         between human-computer interaction and machine learning.</strong></p>
         <p id='current-work'>Currently, I'm a research assistant at the <a href="https://grouplens.org/" target="_blank">GroupLens lab</a>, advised by <a href="https://cse.umn.edu/cs/stevie-chancellor" target="_blank">Stevie Chancellor</a>.
-        There, I'm helping build a full-stack application and chatbot that helps facilitate virtual meetings. I'm was also a teaching assistant for <a href="https://umtc.catalog.prod.coursedog.com/courses/8103461" target="_blank">CSCI 1933</a>, my old university's
+        There, I'm helping build a full-stack application and chatbot that helps facilitate virtual meetings. I was also a teaching assistant for <a href="https://umtc.catalog.prod.coursedog.com/courses/8103461" target="_blank">CSCI 1933</a>, my old university's
         Introduction to Algorithms & Data Structures Course, and have taught <a href="https://umtc.catalog.prod.coursedog.com/courses/8096661" target="_blank">CSCI 1133</a>, the Introduction
         to Computing course.</p>
         <p>I'm on the lookout for opportunities in software engineering for Summer 2024. If you'd like
